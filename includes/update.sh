aac1047aaad3c5eedd208bcc5428726ac29d0d0d
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-08-21 21:15:13 +0200

    update updater

--- includes/update.sh
+++ includes/update.sh
@@ -2,122 +2,261 @@
 
 update_apps(){
 echo -e "${BWhite}App Updater${Color_Off}"
-[[ -z $timeout ]] && echo -e "Default Timeout: 500" && timeout=500 || echo -e "\nCustom Timeout: $timeout"
+mapfile -t array < <(cli -m csv -c 'app chart_release query name,update_available,human_version,human_latest_version,container_images_update_available,status' | tr -d " \t\r" | grep -E ",true($|,)" | sort)
+echo -e "🅄 🄿 🄳 🄰 🅃 🄴 🅂"
+[[ -z ${array[*]} ]] && echo "There are no updates available" && echo -e "\n" && return 0 || echo "Update(s) Available: ${#array[@]}"
+echo "Asynchronous Updates: $update_limit"
+[[ -z $timeout ]] && echo "Default Timeout: 500" && timeout=500 || echo "Custom Timeout: $timeout"
 [[ "$timeout" -le 120 ]] && echo "Warning: Your timeout is set low and may lead to premature rollbacks or skips"
+pool=$(cli -c 'app kubernetes config' | grep -E "dataset\s\|" | awk -F '|' '{print $3}' | awk -F '/' '{print $1}' | tr -d " \t\n\r")
 
-echo ""
-echo "Creating list of Apps to update..."
-
-# Render a list of ignored applications, so users can verify their ignores got parsed correctly.
-if [[ -z ${ignore[*]} ]]; then
-  echo "No apps added to ignore list, continuing..."
-else
-  echo "ignored applications:"
-  for ignored in "${ignore[@]}"
-  do
-    echo "${ignored}"
-  done
-fi
-echo ""
-
-mapfile -t array < <(cli -m csv -c 'app chart_release query name,update_available,human_version,human_latest_version,container_images_update_available,status' | grep -E ",true(,|$)" | sort)
-[[ -z ${array[*]} ]] && echo -e "\nThere are no updates available or middleware timed out" && return 0 || echo -e "\n${#array[@]} update(s) available:"
-PIDlist=()
-
-# Draft a list of app names, seperate from actuall execution
-# This prevents outputs getting mixed together
-for i in "${array[@]}"
+it=0
+while_count=0
+rm deploying 2>/dev/null
+while true
 do
-  app_name=$(echo "$i" | awk -F ',' '{print $1}') #print out first catagory, name.
-  echo "$app_name"
+    if while_status=$(cli -m csv -c 'app chart_release query name,update_available,human_version,human_latest_version,status' 2>/dev/null) ; then
+        ((while_count++))
+        [[ -z $while_status ]] && continue || echo -e "$while_count\n$while_status" > all_app_status
+        mapfile -t deploying_check < <(grep ",DEPLOYING," all_app_status)
+        for i in "${deploying_check[@]}"
+        do
+            app_name=$(echo "$i" | awk -F ',' '{print $1}')
+            [[ ! -e deploying ]] && touch deploying
+            if ! grep -qs "$app_name,DEPLOYING" deploying; then
+                echo "$app_name,DEPLOYING" >> deploying
+            fi
+        done
+    else
+        echo "Middlewared timed out. Consider setting a lower number for async applications"
+        continue
+    fi
+    proc_count=${#processes[@]}
+    count=0
+    for proc in "${processes[@]}"
+    do
+        kill -0 "$proc" &> /dev/null || { unset "processes[$count]"; ((proc_count--)); }
+        ((count++))
+    done
+    if [[ "$proc_count" -ge "$update_limit" ]]; then
+        sleep 3
+    elif [[ $it -lt ${#array[@]} ]]; then
+        # loop=0
+        # until [[ $loop -ge 2 || $it -ge ${#array[@]} ]];
+        # do
+        pre_process "${array[$it]}" &
+        processes+=($!)
+        ((it++))
+        # ((loop++))
+        # done
+    elif [[ $proc_count != 0 ]]; then # Wait for all processes to finish
+        sleep 3
+    else # All processes must be completed, break out of loop
+        break
+    fi
 done
-
-echo ""
-echo "Updating Apps..."
-
-# Create a background task for each update as async solution
-for i in "${array[@]}"
-do
-  executeUpdate "${i}" &
-  PIDlist+=($!)
-done
-echo ""
-echo "Waiting for update results..."
-
-# Wait for all the async updates to complete
-for p in "${PIDlist[@]}"
-do
-  wait "${p}" ||:
-done
-
+rm all_app_status 2>/dev/null
+rm deploying 2>/dev/null
+echo
+echo
 }
 export -f update_apps
 
 
-
-# This is a combination of stopping previously-stopped apps and apps stuck Deploying after update
-after_update_actions(){
-SECONDS=0
-count=0
-sleep 15
-
-# Keep this running and exit the endless-loop based on a timer, instead of a countered-while-loop
-# shellcheck disable=SC2050
-while [[ "0"  !=  "1" ]]
-do
-    (( count++ ))
-    status=$(cli -m csv -c 'app chart_release query name,update_available,human_version,human_latest_version,status' | grep "^$app_name," | awk -F ',' '{print $2}')
-    if [[ "$status"  ==  "ACTIVE" && "$startstatus"  ==  "STOPPED" ]]; then
-        [[ "$verbose" == "true" ]] && echo "Returing to STOPPED state.."
-        midclt call chart.release.scale "$app_name" '{"replica_count": 0}' &> /dev/null && echo "Stopped"|| echo "FAILED"
-        break
-    elif [[ "$SECONDS" -ge "$timeout" && "$status"  ==  "DEPLOYING" && "$failed" != "true" ]]; then
-        echo -e "Error: Run Time($SECONDS) for $app_name has exceeded Timeout($timeout)\nIf this is a slow starting application, set a higher timeout with -t\nIf this applicaion is always DEPLOYING, you can disable all probes under the Healthcheck Probes Liveness section in the edit configuration\nReverting update.."
-        midclt call chart.release.rollback "$app_name" "{\"item_version\": \"$rollback_version\"}" &> /dev/null
-        [[ "$startstatus"  ==  "STOPPED" ]] && failed="true" && after_update_actions && unset failed #run back after_update_actions function if the app was stopped prior to update
-        break
-    elif [[ "$SECONDS" -ge "$timeout" && "$status"  ==  "DEPLOYING" && "$failed" == "true" ]]; then
-        echo -e "Error: Run Time($SECONDS) for $app_name has exceeded Timeout($timeout)\nThe application failed to be ACTIVE even after a rollback,\nManual intervention is required\nAbandoning"
-        break
-    elif [[ "$status"  ==  "STOPPED" ]]; then
-        [[ "$count" -le 1 && "$verbose" == "true"  ]] && echo "Verifying Stopped.." && sleep 15 && continue #if reports stopped on FIRST time through loop, double check
-        [[ "$count" -le 1  && -z "$verbose" ]] && sleep 15 && continue #if reports stopped on FIRST time through loop, double check
-        echo "Stopped" && break #if reports stopped any time after the first loop, assume its extermal services.
-    elif [[ "$status"  ==  "ACTIVE" ]]; then
-        [[ "$count" -le 1 && "$verbose" == "true"  ]] && echo "Verifying Active.." && sleep 15 && continue #if reports active on FIRST time through loop, double check
-        [[ "$count" -le 1  && -z "$verbose" ]] && sleep 15 && continue #if reports active on FIRST time through loop, double check
-        echo "Active" && break #if reports active any time after the first loop, assume actually active.
+pre_process(){
+app_name=$(echo "${array[$it]}" | awk -F ',' '{print $1}') #print out first catagory, name.
+printf '%s\0' "${ignore[@]}" | grep -iFxqz "${app_name}" && echo -e "\n$app_name\nIgnored, skipping" && return 0 #If application is on ignore list, skip
+old_app_ver=$(echo "${array[$it]}" | awk -F ',' '{print $4}' | awk -F '_' '{print $1}' | awk -F '.' '{print $1}') #previous/current Application MAJOR Version
+new_app_ver=$(echo "${array[$it]}" | awk -F ',' '{print $5}' | awk -F '_' '{print $1}' | awk -F '.' '{print $1}') #new Application MAJOR Version
+old_chart_ver=$(echo "${array[$it]}" | awk -F ',' '{print $4}' | awk -F '_' '{print $2}' | awk -F '.' '{print $1}') # Old Chart MAJOR version
+new_chart_ver=$(echo "${array[$it]}" | awk -F ',' '{print $5}' | awk -F '_' '{print $2}' | awk -F '.' '{print $1}') # New Chart MAJOR version
+startstatus=$(echo "${array[$it]}" | awk -F ',' '{print $2}') #status of the app: STOPPED / DEPLOYING / ACTIVE
+diff_app=$(diff <(echo "$old_app_ver") <(echo "$new_app_ver")) #caluclating difference in major app versions
+diff_chart=$(diff <(echo "$old_chart_ver") <(echo "$new_chart_ver")) #caluclating difference in Chart versions
+[[ "$diff_app" != "$diff_chart" && $update_apps == "true" ]] && echo -e "\n$app_name\nMajor Release, update manually" && return
+old_full_ver=$(echo "${array[$it]}" | awk -F ',' '{print $4}') #Upgraded From
+new_full_ver=$(echo "${array[$it]}" | awk -F ',' '{print $5}') #Upraded To
+rollback_version=$(echo "${array[$it]}" | awk -F ',' '{print $4}' | awk -F '_' '{print $2}')
+if  grep -qs "^$app_name," failed 2>/dev/null; then
+    failed_ver=$(grep "^$app_name," failed | awk -F ',' '{print $2}')
+    if [[ "$failed_ver" == "$new_full_ver" ]] ; then
+        echo -e "\n$app_name"
+        echo -e "Skipping previously failed version:\n$new_full_ver"
+        return 0
     else
-        [[ "$verbose" == "true" ]] && echo "Waiting $((timeout-SECONDS)) more seconds for $app_name to be ACTIVE"
-        sleep 15
-        continue
+        sed -i /"$app_name",/d failed
+    fi
+fi
+
+[[ ! -e external_services ]] && touch external_services
+if ! grep -qs "^$app_name," external_services ; then
+    if ! grep qs "/external-service" /mnt/"$pool"/ix-applications/releases/"$app_name"/charts/"$(find /mnt/"$pool"/ix-applications/releases/"$app_name"/charts/ -maxdepth 1 -type d -printf '%P\n' | sort -r | head -n 1)"/Chart.yaml 2>/dev/null; then
+        echo "$app_name,false" >> external_services
+    else
+        echo "$app_name,true" >> external_services
+    fi
+fi
+
+echo_array+=("\n$app_name")
+if [[ $stop_before_update == "true" && "$startstatus" !=  "STOPPED" ]]; then # Check to see if user is using -S or not
+    [[ "$verbose" == "true" ]] && echo_array+=("Stopping prior to update..")
+    if stop_app ; then
+        echo_array+=("Stopped")
+    else
+        echo_array+=("Error: Failed to stop $app_name")
+        echo_array
+        return 1
+    fi
+fi
+[[ "$verbose" == "true" ]] && echo_array+=("Updating..")
+if update_app ;then
+    echo_array+=("Updated\n$old_full_ver\n$new_full_ver")
+else
+    echo_array+=("Failed to update")
+    echo_array
+    return
+fi
+if grep -qs "^$app_name,true" external_services ; then
+    echo_array
+    return
+else
+    after_update_actions
+fi
+}
+export -f pre_process
+
+
+update_app(){
+current_loop=0
+while true
+do
+    update_avail=$(grep "^$app_name," all_app_status | awk -F ',' '{print $3}')
+    if [[ $update_avail == "true" ]]; then
+        if ! cli -c 'app chart_release upgrade release_name=''"'"$app_name"'"' &> /dev/null ; then
+            before_loop=$(head -n 1 all_app_status)
+            current_loop=0
+            until [[ "$(grep "^$app_name," all_app_status | awk -F ',' '{print $3}')" != "$update_avail" ]]   # Wait for a specific change to app status, or 3 refreshes of the file to go by.
+            do
+                if [[ $current_loop -gt 2 ]]; then
+                    cli -c 'app chart_release upgrade release_name=''"'"$app_name"'"' &> /dev/null || return 1     # After waiting, attempt an update once more, if fails, return error code
+                elif ! echo -e "$(head -n 1 all_app_status)" | grep -qs ^"$before_loop" ; then                # The file has been updated, but nothing changed specifically for the app.
+                    before_loop=$(head -n 1 all_app_status)
+                    ((current_loop++))
+                fi
+                sleep 1
+            done
+        fi
+        break
+    elif [[ $update_avail == "false" ]]; then
+        break
+    else
+        sleep 3
     fi
 done
 }
+export -f update_app
+
+
+stop_app(){
+count=0
+while [[ "$status" !=  "STOPPED" ]]
+do
+    status=$( grep "^$app_name," all_app_status | awk -F ',' '{print $2}')
+    if [[ $count -gt 2 ]]; then # If failed to stop app 3 times, return failure to parent shell
+        return 1
+    elif ! cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": 0}' &> /dev/null ; then
+        before_loop=$(head -n 1 all_app_status)
+        ((count++))
+        until [[ $(head -n 1 all_app_status) != "$before_loop" ]] # Upon failure, wait for status update before continuing
+        do
+            sleep 1
+        done
+    else
+        break
+    fi
+done
+}
+export -f stop_app
+
+
+after_update_actions(){
+SECONDS=0
+count=0
+if [[ $rollback == "true" || "$startstatus"  ==  "STOPPED" ]]; then
+    while true
+    do
+        status=$(grep "^$app_name," all_app_status | awk -F ',' '{print $2}')
+        if [[ $count -lt 1 && $status == "ACTIVE" && "$(grep "^$app_name," deploying 2>/dev/null | awk -F ',' '{print $2}')" != "DEPLOYING" ]]; then                # If status shows up as Active or Stopped on the first check, verify that. Otherwise it may be a false report..
+            [[ "$verbose" == "true" ]] && echo_array+=("Verifying $status..")
+            before_loop=$(head -n 1 all_app_status)
+            current_loop=0
+            until [[ "$status" != "ACTIVE" || $current_loop -gt 4 ]] # Wait for a specific change to app status, or 3 refreshes of the file to go by.
+            do
+                status=$(grep "^$app_name," all_app_status | awk -F ',' '{print $2}')
+                sleep 1
+                if ! echo -e "$(head -n 1 all_app_status)" | grep -qs ^"$before_loop" ; then
+                    before_loop=$(head -n 1 all_app_status)
+                    ((current_loop++))
+                fi
+            done
+        fi
+        (( count++ ))
+        if [[ "$status"  ==  "ACTIVE" ]]; then
+            if [[ "$startstatus"  ==  "STOPPED" ]]; then
+                [[ "$verbose" == "true" ]] && echo_array+=("Returing to STOPPED state..")
+                if stop_app ; then
+                    echo_array+=("Stopped")
+                else
+                    echo_array+=("Error: Failed to stop $app_name")
+                    echo_array
+                    return 1
+                fi
+                break
+            else
+                echo_array+=("Active")
+                break
+            fi
+        elif [[ "$SECONDS" -ge "$timeout" ]]; then
+            if [[ $rollback == "true" ]]; then
+                if [[ "$failed" != "true" ]]; then
+                    echo_array+=("Error: Run Time($SECONDS) for $app_name has exceeded Timeout($timeout)")
+                    echo_array+=("If this is a slow starting application, set a higher timeout with -t")
+                    echo_array+=("If this applicaion is always DEPLOYING, you can disable all probes under the Healthcheck Probes Liveness section in the edit configuration")
+                    echo_array+=("Reverting update..")
+                    midclt call chart.release.rollback "$app_name" "{\"item_version\": \"$rollback_version\"}" &> /dev/null || { echo_array+=("Error: Failed to rollback $app_name") ; break ; }
+                    [[ "$startstatus"  ==  "STOPPED" ]] && failed="true" && after_update_actions #run back after_update_actions function if the app was stopped prior to update
+                    echo "$app_name,$new_full_ver" >> failed
+                    break
+                else
+                    echo_array+=("Error: Run Time($SECONDS) for $app_name has exceeded Timeout($timeout)")
+                    echo_array+=("The application failed to be ACTIVE even after a rollback")
+                    echo_array+=("Manual intervention is required\nAbandoning")
+                    break
+                fi
+            else
+                echo_array+=("Error: Run Time($SECONDS) for $app_name has exceeded Timeout($timeout)")
+                echo_array+=("If this is a slow starting application, set a higher timeout with -t")
+                echo_array+=("If this applicaion is always DEPLOYING, you can disable all probes under the Healthcheck Probes Liveness section in the edit configuration")
+                break
+            fi
+        else
+            [[ "$verbose" == "true" ]] && echo_array+=("Waiting $((timeout-SECONDS)) more seconds for $app_name to be ACTIVE")
+            sleep 5
+            continue
+        fi
+    done
+fi
+echo_array
+}
 export -f after_update_actions
 
-# Determine what all the required information for the App to update, check it and execute the update using the SCALE API
-executeUpdate(){
-  app_name=$(echo "$1" | awk -F ',' '{print $1}') #print out first catagory, name.
-  old_app_ver=$(echo "$1" | awk -F ',' '{print $4}' | awk -F '_' '{print $1}' | awk -F '.' '{print $1}') #previous/current Application MAJOR Version
-  new_app_ver=$(echo "$1" | awk -F ',' '{print $5}' | awk -F '_' '{print $1}' | awk -F '.' '{print $1}') #new Application MAJOR Version
-  old_chart_ver=$(echo "$1" | awk -F ',' '{print $4}' | awk -F '_' '{print $2}' | awk -F '.' '{print $1}') # Old Chart MAJOR version
-  new_chart_ver=$(echo "$1" | awk -F ',' '{print $5}' | awk -F '_' '{print $2}' | awk -F '.' '{print $1}') # New Chart MAJOR version
-  status=$(echo "$1" | awk -F ',' '{print $2}') #status of the app: STOPPED / DEPLOYING / ACTIVE
-  startstatus=$status
-  diff_app=$(diff <(echo "$old_app_ver") <(echo "$new_app_ver")) #caluclating difference in major app versions
-  diff_chart=$(diff <(echo "$old_chart_ver") <(echo "$new_chart_ver")) #caluclating difference in Chart versions
-  old_full_ver=$(echo "$1" | awk -F ',' '{print $4}') #Upgraded From
-  new_full_ver=$(echo "$1" | awk -F ',' '{print $5}') #Upraded To
-  rollback_version=$(echo "$1" | awk -F ',' '{print $4}' | awk -F '_' '{print $2}')
-  printf '%s\0' "${ignore[@]}" | grep -iFxqz "${app_name}" && echo -e "\n$app_name\nIgnored, skipping" && return #If application is on ignore list, skip
-  if [[ "$diff_app" == "$diff_chart" || "$update_all_apps" == "true" ]]; then #continue to update
-          [[ "$verbose" == "true" ]] && echo "Updating.."
-          # shellcheck disable=SC2015
-          cli -c 'app chart_release upgrade release_name=''"'"$app_name"'"' &> /dev/null && echo -e "Updated $app_name\n$old_full_ver\n$new_full_ver" && after_update_actions || { echo -e "$app_name: update ${IRed}FAILED${Color_Off}"; return; }
-  else
-      echo -e "\n$app_name\nMajor Release, update manually"
-      return
-  fi
+
+echo_array(){
+#Dump the echo_array, ensures all output is in a neat order.
+for i in "${echo_array[@]}"
+do
+    echo -e "$i"
+done
+
 }
-export -f executeUpdate
+export -f echo_array
